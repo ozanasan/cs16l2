c7446b8d3d74b7dbb21361183d4771b7544486c2
Ozan Asan <ozanasan@Ozans-MBP-2.attlocal.net>
2016-05-03 20:56:03 -0700

    ozan : lesson 1

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Ozan Asan on 5/2/16.
+//  Copyright © 2016 Ozan Asan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBAction func touchDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
+        
+        
+    }
+    
+    @IBAction func performOperation(sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            if mathematicalSymbol == "π" {
+                display.text = String(M_PI)
+            }
+        }
+    }
+}
+
